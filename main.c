acb4077c66548c7c9c2f3b3fed67d7ed1f20c68b
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-03-11 07:39:31 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//ESERCIZIO:
+//DARE IN INGRESSO STRINGHE NUMERICHE
+//E FARE LA SOMMA DI TUTTE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int i;
+
+int main(int argc, char *argv[])
+{
+  int n=0, somma=0;
+        for(i=0;i<argc;i++)
+          { 
